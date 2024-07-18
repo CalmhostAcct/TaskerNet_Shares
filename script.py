174d71ca1ceeefe918292c636c3fa4e876a04f96
CalmhostAcct <backuptabletinfo@gmail.com>
2024-07-18 18:56:02 -0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,33 @@
+import requests
+import json
+with open("taskernet_shares.csv", "a") as myfile:
+   print("Name, Url")
+   myfile.write("Name, Url  \n")
+   myfile.close()
+
+def taskernet(page_token=None):
+    base_url = "https://taskernet.com/_ah/api/datashare/v1/shares/public"
+    if page_token:
+        request = requests.get(base_url + "?a=0&tags=&pageToken=" + page_token)
+    else:
+        request = requests.get(base_url)
+    
+    data = json.loads(request.content)
+    shares = data["shares"]
+    
+    for share in shares:
+        if shares:
+            with open("taskernet_shares.csv", "a") as myfile:
+                myfile.write(f"{share['name']}, {share['url']}\n")
+                print(f" {share['name']}, {share['url']}")
+                myfile.close()
+        else:
+            pass
+    
+    if "pageToken" in data:
+        page_token_b = data["pageToken"]
+        taskernet(page_token_b)
+    else:
+        print("No shares found")
+
+taskernet()
